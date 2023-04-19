fb113f8a99dcbb6fcf9fbeed25cf4002ae58a3c5
Yuqiao Gong <gyq123@sjtu.edu.cn>
2023-04-19 11:39:53 +0800

    modify setup

--- setup.py
+++ setup.py
@@ -8,7 +8,12 @@ with open("README.md", "r") as fh:
 setup(
     name="scBC",
     version=__version__,
-    packages=find_packages(include=["data"]),
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     include_package_data=True,
     install_requires=[
         'scvi-tools',
